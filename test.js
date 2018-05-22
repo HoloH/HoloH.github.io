865a48cd7182962fe39f2fff0343224a572518ec
HoloH <hanne.holo@gmail.com>
2018-05-22 17:55:27 +0200

    testing 2 methods for click expand method

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 $('.expandContent').click(function() {
     $('showCV').show();
 });
+
+function myCV() {
+    document.getElementsById("expandContent").show.myCV;
+}
+
+function myCV() {
+    document.getElementsByClassName("myCV").show
