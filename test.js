ea4edd7ac75817885ad88c345ca932c94d71c19c
HoloH <hanne.holo@gmail.com>
2018-05-22 17:28:21 +0200

    editing expand to show cv element

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-console.log("My code is loaded and running.");
-
-setTimeout(function() {
-    document.querySelector("h1").style.color = "#ff0000";
-}, 30000);
-
-
-function clickonCV() {
-    alert("You are now opening my CV");
-}
-
-object.onclick = function() {myCV};
+$('.expandContent').click(function() {
+    $('showCV').show();
+});
