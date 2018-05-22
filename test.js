05b11182de3df520c8ec988804c62bbdb15aba91
HoloH <hanne.holo@gmail.com>
2018-05-22 17:19:44 +0200

    trying to add expanson element for CV

--- test.js
+++ test.js
@@ -4,3 +4,9 @@ setTimeout(function() {
     document.querySelector("h1").style.color = "#ff0000";
 }, 30000);
 
+
+function clickonCV() {
+    alert("You are now opening my CV");
+}
+
+object.onclick = function() {myCV};
