395f3b4f4b938e402486824a6e99a915b21a55e5
HoloH <hanne.holo@gmail.com>
2018-05-23 20:08:30 +0200

    button

---
+++ test.js
@@ -0,0 +1,10 @@
+$(function) {
+
+    function scrollTo(target){
+        if(target.lenght){
+            $("html, body").stop().animate( { scrollTop: target.offset().top }, 1500);
+        }
+    }
+
+    scrollTo( $("#cvSection"));
+}
